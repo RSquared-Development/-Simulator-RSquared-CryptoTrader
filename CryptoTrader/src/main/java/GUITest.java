fd64b3996b926b5b5b65798548e1f046386e81a0
rocketrice <provincerice@gmail.com>
2018-01-11 08:55:50 -0600

    Changed some gui stuff

--- CryptoTrader/src/main/java/GUITest.java
+++ CryptoTrader/src/main/java/GUITest.java
@@ -1,7 +1,34 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUITest {
     private JButton button_Account;
     private JButton button_Main;
     private JButton button_Settings;
+
+
+    public GUITest() {
+        button_Main.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: make it switch to Main panel
+            }
+        });
+        button_Settings.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: make it switch to Settings Panel
+            }
+        });
+        button_Account.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: make it switch to Account Settings Panel
+            }
+        });
+    }
+
+    
+
 }
