bc2b1c627867fa0927431daedac059049e87e2ae
rocketrice <provincerice@gmail.com>
2018-01-17 10:43:06 -0600

    tabs

--- CryptoTrader/src/main/java/GUITest.java
+++ CryptoTrader/src/main/java/GUITest.java
@@ -1,3 +1,7 @@
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -34,6 +38,7 @@ public class GUITest {
                 //TODO: make it switch to Main panel
                 switchScreens("home");
                 page = "home";
+                out.println("Page Change Success");
 
             }
         });
@@ -43,6 +48,7 @@ public class GUITest {
                 //TODO: make it switch to Settings Panel
                 switchScreens("settings");
                 page = "settings";
+                out.println("Page Change Success");
             }
         });
         button_Account.addActionListener(new ActionListener() {
@@ -51,6 +57,7 @@ public class GUITest {
                 //TODO: make it switch to Account Settings Panel
                 switchScreens("account");
                 page = "account";
+                out.println("Page Change Success");
             }
         });
         button_Start.addActionListener(new ActionListener() {
@@ -86,26 +93,20 @@ public class GUITest {
 
         if (screen == "home") {
             //set which screens
-            frame.getContentPane().removeAll();
-            frame.getContentPane().add(Outter);
+            frame.setContentPane(new GUITest().Outter);
+            frame.pack();
+            frame.setVisible(true);
             //colors
-            button_Main.setBackground(new Color(0, 106, 71));
-            button_Settings.setBackground(new Color(0, 187, 124));
-            button_Account.setBackground(new Color(0, 187, 124));
         } else if (screen == "settings") {
             //set which screens
             frame.setContentPane(new CurrencySettings().Settings_Outter);
+            frame.pack();
+            frame.setVisible(true);
             //colors
-            button_Settings.setBackground(new Color(0, 106, 71));
-            button_Main.setBackground(new Color(0, 187, 124));
-            button_Account.setBackground(new Color(0, 187, 124));
         } else if (screen == "account") {
             //set which screens
 
             //colors
-            button_Account.setBackground(new Color(0, 106, 71));
-            button_Settings.setBackground(new Color(0, 187, 124));
-            button_Main.setBackground(new Color(0, 187, 124));
         }
     }
 
@@ -125,69 +126,69 @@ public class GUITest {
      */
     private void $$$setupUI$$$() {
         Outter = new JPanel();
-        Outter.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(4, 2, new Insets(0, 0, 0, 0), -1, -1));
+        Outter.setLayout(new GridLayoutManager(4, 2, new Insets(0, 0, 0, 0), -1, -1));
         Outter.setBackground(new Color(-16747077));
         Outter.setForeground(new Color(-16747077));
         Outter.setMinimumSize(new Dimension(1280, 720));
         Outter.setPreferredSize(new Dimension(1280, 720));
-        final com.intellij.uiDesigner.core.Spacer spacer1 = new com.intellij.uiDesigner.core.Spacer();
-        Outter.add(spacer1, new com.intellij.uiDesigner.core.GridConstraints(0, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        Outter.add(spacer1, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         button_Account = new JButton();
         button_Account.setBackground(new Color(-16729220));
         Font button_AccountFont = this.$$$getFont$$$("Verdana", Font.PLAIN, 30, button_Account.getFont());
         if (button_AccountFont != null) button_Account.setFont(button_AccountFont);
         button_Account.setForeground(new Color(-16777216));
         button_Account.setText("Account");
-        Outter.add(button_Account, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 255), null, 0, false));
+        Outter.add(button_Account, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 255), null, 0, false));
         button_Main = new JButton();
         button_Main.setBackground(new Color(-16750009));
         Font button_MainFont = this.$$$getFont$$$("Verdana", Font.PLAIN, 30, button_Main.getFont());
         if (button_MainFont != null) button_Main.setFont(button_MainFont);
         button_Main.setForeground(new Color(-16777216));
         button_Main.setText("Home");
-        Outter.add(button_Main, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 225), null, 0, false));
+        Outter.add(button_Main, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 225), null, 0, false));
         button_Settings = new JButton();
         button_Settings.setBackground(new Color(-16729220));
         Font button_SettingsFont = this.$$$getFont$$$("Verdana", Font.PLAIN, 30, button_Settings.getFont());
         if (button_SettingsFont != null) button_Settings.setFont(button_SettingsFont);
         button_Settings.setForeground(new Color(-16777216));
         button_Settings.setText("Settings");
-        Outter.add(button_Settings, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 225), null, 0, false));
+        Outter.add(button_Settings, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 225), null, 0, false));
         content = new JPanel();
-        content.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-        Outter.add(content, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 3, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        content.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
+        Outter.add(content, new GridConstraints(1, 1, 3, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         content_Home = new JPanel();
-        content_Home.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
+        content_Home.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
         content_Home.setBackground(new Color(-9079691));
         content_Home.setEnabled(true);
         content_Home.setVisible(true);
-        content.add(content_Home, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        content.add(content_Home, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         content_Home.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(-16777216)), null));
         table_Transactions = new JTable();
         table_Transactions.setEnabled(false);
         Font table_TransactionsFont = this.$$$getFont$$$("Verdana", -1, 20, table_Transactions.getFont());
         if (table_TransactionsFont != null) table_Transactions.setFont(table_TransactionsFont);
-        content_Home.add(table_Transactions, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 2, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_BOTH, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        content_Home.add(table_Transactions, new GridConstraints(1, 0, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
         button_Stop = new JButton();
         button_Stop.setBackground(new Color(-16747077));
         Font button_StopFont = this.$$$getFont$$$("Verdana", -1, 30, button_Stop.getFont());
         if (button_StopFont != null) button_Stop.setFont(button_StopFont);
         button_Stop.setForeground(new Color(-16777216));
         button_Stop.setText("Stop");
-        content_Home.add(button_Stop, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        content_Home.add(button_Stop, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         button_Start = new JButton();
         button_Start.setBackground(new Color(-16747077));
         Font button_StartFont = this.$$$getFont$$$("Verdana", -1, 30, button_Start.getFont());
         if (button_StartFont != null) button_Start.setFont(button_StartFont);
         button_Start.setForeground(new Color(-16777216));
         button_Start.setText("Start");
-        content_Home.add(button_Start, new com.intellij.uiDesigner.core.GridConstraints(1, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        content_Home.add(button_Start, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         accountNetWorth17TextField = new JTextField();
         accountNetWorth17TextField.setEditable(false);
         Font accountNetWorth17TextFieldFont = this.$$$getFont$$$("Verdana", Font.BOLD, 48, accountNetWorth17TextField.getFont());
         if (accountNetWorth17TextFieldFont != null) accountNetWorth17TextField.setFont(accountNetWorth17TextFieldFont);
         accountNetWorth17TextField.setText("Account Net Worth: 17.46578 BTC");
-        content_Home.add(accountNetWorth17TextField, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        content_Home.add(accountNetWorth17TextField, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
     }
 
     /**
