cfe0e5e0abf9f09553037c1e1de2adf956123e22
Unknown <ruben.s.ruiz@outlook.com>
2018-01-31 11:58:51 -0600

    I think I got it to center on any screen

--- CryptoTrader/src/main/java/Gui/R2Splash.java
+++ CryptoTrader/src/main/java/Gui/R2Splash.java
@@ -8,11 +8,22 @@ public class R2Splash {
     private static JWindow splashScreen = new JWindow();
     private static Image favicon;
 
-    public static void showSplash() {
 
-        favicon = Toolkit.getDefaultToolkit().getImage("RSquared_Logo.png");
-        splashScreen.getContentPane().add(new JLabel("", new ImageIcon("RSquared_Logo.png"), SwingConstants.LEFT));
-        splashScreen.setBounds((1920)/2-360, ((1080-100)/2), 720, 200);
+    public static void showSplash() {
+        GridLayout test   = new GridLayout(3,3);
+        JLabel iconHolder = new JLabel("", new ImageIcon("RSquared_Logo.png"), SwingConstants.LEFT);
+
+        double hWidth  = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        double hHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        JPanel test2 = new JPanel();
+        splashScreen.setContentPane(test2);
+
+        //favicon = Toolkit.getDefaultToolkit().getImage("RSquared_Logo.png");
+        //splashScreen.getContentPane().add( iconHolder);
+        //splashScreen.getContentPane().add(new JLabel("RSquared Cryptocurrency Trading App", SwingConstants.CENTER));
+        //splashScreen.getContentPane().add(new JLabel("Automate your trades today",SwingConstants.));
+        //splashScreen.getContentPane().add(new JLabel("RESTRICTED ALPHA"), SwingConstants.RIGHT);
+        splashScreen.setBounds(((int)hWidth)/2-360, (((int)hHeight-100)/2), 720, 200);
         splashScreen.setIconImage(favicon);
         splashScreen.setVisible(true);
 
