c49e5e9951ce2678c823a69b2003e0c9cb0ca29a
Unknown <ruben.s.ruiz@outlook.com>
2018-01-31 12:08:27 -0600

    Splash Screen Work

--- CryptoTrader/src/main/java/Gui/R2Splash.java
+++ CryptoTrader/src/main/java/Gui/R2Splash.java
@@ -11,16 +11,19 @@ public class R2Splash {
 
     public static void showSplash() {
         GridLayout test   = new GridLayout(3,3);
-        JLabel iconHolder = new JLabel("", new ImageIcon("RSquared_Logo.png"), SwingConstants.LEFT);
+        JLabel iconHolder = new JLabel("", new ImageIcon("RSquared_Logo.png"), SwingConstants.CENTER);
 
         double hWidth  = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         double hHeight = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
         JPanel test2 = new JPanel();
+        favicon = Toolkit.getDefaultToolkit().getImage("RSquared_Logo.png");
+        test2.add(iconHolder);
+        test2.add(new JLabel("RSquared Cryptocurrency Trading App"));
         splashScreen.setContentPane(test2);
 
         //favicon = Toolkit.getDefaultToolkit().getImage("RSquared_Logo.png");
         //splashScreen.getContentPane().add( iconHolder);
-        //splashScreen.getContentPane().add(new JLabel("RSquared Cryptocurrency Trading App", SwingConstants.CENTER));
+        //splashScreen.getContentPane().add(new JLabel("RSquared Cryptocurrency Trading App"));
         //splashScreen.getContentPane().add(new JLabel("Automate your trades today",SwingConstants.));
         //splashScreen.getContentPane().add(new JLabel("RESTRICTED ALPHA"), SwingConstants.RIGHT);
         splashScreen.setBounds(((int)hWidth)/2-360, (((int)hHeight-100)/2), 720, 200);
